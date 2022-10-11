bdd88aacb2ca9fe1153d8d91d17dd20abbaa3404
Pablo <pablohuggem@gmail.com>
2022-10-11 16:34:41 -0300

    First Commit

---
+++ xmake.lua
@@ -0,0 +1,90 @@
+set_languages("c++23")
+add_rules("mode.debug", "mode.release")
+
+set_warnings("all", "error")
+-- set_optimize("fastest")
+
+-- add libraries
+local external_libs = {"fmt", "cxxopts", "libuv", "v8"}
+add_requires(table.unpack(external_libs))
+add_includedirs("include", "/usr/local/include", "/usr/include")
+
+target("libdone")
+set_kind("static")
+add_files("src/**/*.cpp")
+add_packages(table.unpack(external_libs))
+
+target("done")
+set_kind("binary")
+add_files("src/main.cpp")
+add_packages(table.unpack(external_libs))
+add_deps("libdone")
+
+--
+-- If you want to known more usage about xmake, please see https://xmake.io
+--
+-- ## FAQ
+--
+-- You can enter the project directory firstly before building project.
+--
+--   $ cd projectdir
+--
+-- 1. How to build project?
+--
+--   $ xmake
+--
+-- 2. How to configure project?
+--
+--   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
+--
+-- 3. Where is the build output directory?
+--
+--   The default output directory is `./build` and you can configure the output directory.
+--
+--   $ xmake f -o outputdir
+--   $ xmake
+--
+-- 4. How to run and debug target after building project?
+--
+--   $ xmake run [targetname]
+--   $ xmake run -d [targetname]
+--
+-- 5. How to install target to the system directory or other output directory?
+--
+--   $ xmake install
+--   $ xmake install -o installdir
+--
+-- 6. Add some frequently-used compilation flags in xmake.lua
+--
+-- @code
+--    -- add debug and release modes
+--    add_rules("mode.debug", "mode.release")
+--
+--    -- add macro defination
+--    add_defines("NDEBUG", "_GNU_SOURCE=1")
+--
+--    -- set warning all as error
+--    set_warnings("all", "error")
+--
+--    -- set language: c99, c++11
+--    set_languages("c99", "c++11")
+--
+--    -- set optimization: none, faster, fastest, smallest
+--    set_optimize("fastest")
+--
+--    -- add include search directories
+--    add_includedirs("/usr/include", "/usr/local/include")
+--
+--    -- add link libraries and search directories
+--    add_links("tbox")
+--    add_linkdirs("/usr/local/lib", "/usr/lib")
+--
+--    -- add system link libraries
+--    add_syslinks("z", "pthread")
+--
+--    -- add compilation and link flags
+--    add_cxflags("-stdnolib", "-fno-strict-aliasing")
+--    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
+--
+-- @endcode
+--
